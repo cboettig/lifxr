79afaad4d3f256303e54427e4b50dc56c7fdac66
Carl Boettiger <cboettig@gmail.com>
2015-03-17 11:06:54 -0700

    add documentation

--- R/lifx-cloud.R
+++ R/lifx-cloud.R
@@ -3,33 +3,42 @@
 BASE <- "https://api.lifx.com"
 VERSION <- "v1beta1"
 
-#
+#' ping
+#' 
+#' ping the lifx API and get a status reply
 #' @import httr
 #' @import RJSONIO
+#' @export
 ping <- function(){
   results <- GET(paste0(BASE, "/", VERSION, "/lights.json"), 
                  query = list(access_token = getOption("LIFX_PAT", "")))
   if(results$status_code != 200)
-    message("LIFX server could not be reached")
+    message("LIFX bulbs could not be reached")
   else
-    message("ping! LIFX server is active.")
+    message("ping! LIFX is active.")
 }
 
-
+#' lights
+#' 
+#' list lights, their status and properties
 #' @param selector a string in format '[type]:[value]', where type can be 
 #' 'all', 'id', 'label', 'group', 'group_id', 'location', 'location_id', 
-#' 'scene_id', and value is what you want to target.
+#' 'scene_id', and value is what you want to target. The default is 'all',
+#' which needs no value argument. 
+#' @return a list of all lights, status, and properties 
 #' @export
-#' @details Use the default selector to get a list of all lights and properties
 lights <- function(selector = "all"){
   results <- GET(paste0(BASE, "/", VERSION, "/lights/", selector), 
                  query = list(access_token = getOption("LIFX_PAT", "")))
   RJSONIO::fromJSON(content(results, as="text"))
 }
 
-
-
-
+#' current color
+#' 
+#' list of the current colors, in a format valid for \code{\link{color}}
+#' @inheritParams lights
+#' @return httr response object
+#' @export
 current_color <- function(selector = "all"){
   light_list <- lights(selector = selector)
   sapply(light_list, 
@@ -51,14 +60,25 @@ current_color <- function(selector = "all"){
 }
 
 
-
+#' toggle
+#' 
+#' toggle lights on and off
+#' @inheritParams lights
+#' @return httr response object
 #' @export
 toggle <- function(selector = "all"){
   POST(paste0(BASE, "/", VERSION, "/lights/", selector, "/toggle.json"), 
        query = list(access_token = getOption("LIFX_PAT", "")))
 }
 
-#' @export
+#' power
+#' 
+#' power lights on or off with a fade duration
+#' @param state on or off?
+#' @param duration the length of the effect
+#' @inheritParams lights
+#' @return httr response object
+#' @details Not exported because it conflicts with stats::power. see on() and off()
 power <- function(state = c("on", "off"), selector = "all", duration = 1.0){
   state <- match.arg(state)
   PUT(paste0(BASE, "/", VERSION, "/lights/", selector, "/power.json"), 
@@ -67,20 +87,48 @@ power <- function(state = c("on", "off"), selector = "all", duration = 1.0){
                    access_token = getOption("LIFX_PAT", "")))
 }
 
+
+#' off
+#' 
+#' power lights off with a fade duration
+#' @param duration the length of the effect
+#' @inheritParams lights
+#' @return httr response object
+off <-  function(selector = "all", duration = 1.0){
+  power("off", selector = selector, duration = duration)
+}
+
+
+#' on
+#' 
+#' power lights on 
+#' @param duration the length of the effect
+#' @inheritParams lights
+#' @return httr response object
+on <- function(selector = "all", duration = 1.0){
+  power("on", selector = selector, duration = duration)
+}
+
+
 #' color
 #' 
 #' set lifx color
-#' @param color a string describing the desired color; see examples
+#' @param color a string describing the desired color; see examples.
+#' @param duration the length of the effect
+#' @param power_on should the light be powered on if it is off? (FALSE just leaves light off)
 #' @inheritParams lights
-#' @details Note that the kelvin temperature ranges from 2700 to 8000. 
+#' @details Note that the kelvin temperature ranges from 2700 to 8000. Hue in HSB list is 
+#' a number between 0 and 360, whereas saturation and brightness should be between 0 and 1.
 #' 
-#' @examples
+#' @examples \dontrun{
 #'  color("green", "label:desk")   # deep green, brightness untouched on lights labeled 'desk'
 #'  color("blue brightness:100%")  # deep blue, maximum brightness
 #'  color("hsb:0,1,1")             # deep red, maximum brightness
 #'  color("random")                # random hue, maximum saturation, brightness untouched
 #'  color("kelvin:2700")           # warm white, brightness untouched
 #'  color("saturation:100%")       # set maximum saturation
+#' }
+#' @return httr response object
 #' @export
 color <- function(color, selector="all", duration = 1.0, power_on = TRUE){
   PUT(paste0(BASE, "/", VERSION, "/lights/", selector, "/color.json"), 
@@ -90,8 +138,22 @@ color <- function(color, selector="all", duration = 1.0, power_on = TRUE){
                    access_token = getOption("LIFX_PAT", "")))
 }
 
+
+#' breathe
+#' 
+#' Display a breathe effect
+#' @param from_color Same syntax as color, defaults to current color (of first bulb in selection)
+#' @param period time in seconds for the cycle to take place
+#' @param cycles number of cycles to perform
+#' @param persist should the color persist after the effect? default is FALSE (returns to original color)
+#' @param peak when in the cycle should the color be at it's maximum intensity?
+#' @inheritParams color
+#' @examples \dontrun{
 #' breathe("purple", "blue")
-breathe <- function(color, from_color, period = 10.0, cycles = 2, persist = FALSE,
+#' }
+#' @export
+breathe <- function(color, from_color = current_color(selector)[[1]], 
+                    period = 10.0, cycles = 2, persist = FALSE,
                     peak = 0.5, selector="all", power_on = TRUE){
   settings <- list(color = color, 
                    from_color = from_color,
@@ -105,10 +167,16 @@ breathe <- function(color, from_color, period = 10.0, cycles = 2, persist = FALS
        query = settings)
 }
 
-
-
-pulse <- function(color, from_color, period = 5.0, cycles = 1, persist = FALSE,
-                    duty_cycle = 0.5, selector="all", power_on = TRUE){
+#' pulse
+#' 
+#' pulse a color for a defined period
+#' @inheritParams breathe
+#' @param duty_cycle Ratio of the period where color is active. Only used for pulse. Defaults to 0.5. Range: 0-1
+#' @return httr response object
+#' @export
+pulse <- function(color, from_color = current_color(selector)[[1]], 
+                  period = 5.0, cycles = 1, persist = FALSE,
+                   duty_cycle = 0.5, selector="all", power_on = TRUE){
   settings <- list(color = color, 
                    from_color = from_color,
                    period = period,
@@ -121,14 +189,26 @@ pulse <- function(color, from_color, period = 5.0, cycles = 1, persist = FALSE,
        query = settings)
 }
 
-
-
+#' label
+#' 
+#' add a label to a bulb
+#' @param label the label for the bulb
+#' @param selector selector pattern for a single bubl, e.g. id:<idstring>
+#' @return httr response object
+#' @export
 label <- function(label, selector) {
   PUT(paste0(BASE, "/", VERSION, "/lights/", selector, "/label.json"), 
       query = list(label = label, 
                    access_token = getOption("LIFX_PAT", "")))
 }
 
+#' scene
+#' 
+#' turn on a scene for a bulb
+#' @inheritParams power
+#' @param scene_id the id of the desired scene
+#' @return httr response object
+#' @export
 scene <- function(state = c("on", "off"), scene_id, duration = 1.0){
   state <- match.arg(state)
   PUT(paste0(BASE, "/", VERSION, "/scenes/scene_id:", scene_id, "/power.json"), 
@@ -137,10 +217,19 @@ scene <- function(state = c("on", "off"), scene_id, duration = 1.0){
                    access_token = getOption("LIFX_PAT", "")))
 }
 
+#' parse color
+#'
+#' Parse a color string and return hue, saturation, brightness and kelvin values
+#' @param string The color string to parse
+#' @return hsbk information for the string. NOTE: This API endpoing appears not to be working yet! 
+#' @export
 parse_color <- function(string){
   results <- PUT(paste0(BASE, "/", VERSION, "/color.json"), 
                  query = list(string = string,
                               access_token = getOption("LIFX_PAT", "")))
   RJSONIO::fromJSON(content(results, as="text"))
 }
+
+
+
 
--- R/lifx-server.R
+++ R/lifx-server.R
@@ -1,8 +1,7 @@
-## These functions are designed to work with a local server only.
+## These functions are designed to work with a local server only, which must be running separately
+## These methods are depricated at this time, see lifx-cloud.R functions instead. 
 
-#' @import httr
-#' @import RJSONIO
-ping <- function(){
+server_ping <- function(){
   results <- GET("http://localhost:56780/lights.json")
   if(results$status_code != 200)
     message("LIFX server could not be reached")
@@ -10,46 +9,41 @@ ping <- function(){
     message("ping! LIFX server is active.")
 }
 
-#' 
-status <- function(selector="all"){
+server_status <- function(selector="all"){
   results <- GET(paste0("http://localhost:56780/lights/", selector))
   dat <- RJSONIO::fromJSON(content(results, as="text"))
 }
 
 
-#' 
-on <- function(selector="all"){
+server_on <- function(selector="all"){
   results <- PUT(paste0("http://localhost:56780/lights/", selector, "/on"))
 }
 
-#' 
-off <- function(selector="all"){
+server_off <- function(selector="all"){
   results <- PUT(paste0("http://localhost:56780/lights/", selector, "/off"))
 }
 
 
-#' 
-toggle <- function(selector="all"){
+server_toggle <- function(selector="all"){
   results <- PUT(paste0("http://localhost:56780/lights/", selector, "/toggle"))
 }
 
-#' 
-color <- function(selector="all", hue, saturation=1, brightness=1, duration=1){
+server_color <- function(selector="all", hue, saturation=1, brightness=1, duration=1){
   settings <- RJSONIO::toJSON(list(hue=hue, saturation=saturation, brightness=brightness, duration=duration))
   results <- PUT(paste0("http://localhost:56780/lights/", selector, "/color"), body=settings, encode="json")
 }
 
-#' Briefly set light(s) to green, then restore previous setting
-#' @param selector which lights should we toggle?
-#' @param duration time in seconds for how long should the light be green?
-#' 
-success <- function(selector="all", duration=Inf){
-  current <- status(selector)
-  color(selector, hue=120, saturation=1, brightness=1, duration=0.1) # duration is speed of change
+# Briefly set light(s) to green, then restore previous setting
+# @param selector which lights should we toggle?
+# @param duration time in seconds for how long should the light be green?
+# 
+server_success <- function(selector="all", duration=Inf){
+  current <- server_status(selector)
+  server_color(selector, hue=120, saturation=1, brightness=1, duration=0.1) # duration is speed of change
   if(duration < Inf){
     Sys.sleep(duration)
     # FIXME restores all bulbs to the property of the first bulb
-    color(selector, 
+    server_color(selector, 
           hue = current[[1]]$color[["hue"]], 
           saturation = current[[1]]$color[["saturation"]],
           brightness = current[[1]]$color[["brightness"]],
@@ -57,17 +51,17 @@ success <- function(selector="all", duration=Inf){
   }
 }
 
-#' Briefly set light(s) to red
-#' @param selector which lights should we toggle?
-#' @param duration time in seconds for how long should the light be green?
-#' 
-fail <-  function(selector="all", duration=Inf){
-  current <- status(selector)
-  color(selector, hue=1, saturation=1, brightness=1, duration=0.1) # duration is speed of change
+# Briefly set light(s) to red
+# @param selector which lights should we toggle?
+# @param duration time in seconds for how long should the light be green?
+# 
+server_fail <-  function(selector="all", duration=Inf){
+  current <- server_status(selector)
+  server_color(selector, hue=1, saturation=1, brightness=1, duration=0.1) # duration is speed of change
   if(duration < Inf){
     Sys.sleep(duration)
     # FIXME restores all bulbs to the property of the first bulb
-    color(selector, 
+    server_color(selector, 
           hue = current[[1]]$color[["hue"]], 
           saturation = current[[1]]$color[["saturation"]],
           brightness = current[[1]]$color[["brightness"]],
